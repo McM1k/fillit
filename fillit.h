a7cce662e29acb7d3eca70575845d7b6beede5bc
Valentin ROUSSEAU <vroussea@e2r7p22.42.fr>
2016-01-06 18:53:03 +0100

    norm error in fillit.h

--- fillit.h
+++ fillit.h
@@ -6,13 +6,14 @@
 /*   By: gboudrie <gboudrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/06 17:37:38 by gboudrie          #+#    #+#             */
-/*   Updated: 2016/01/06 17:52:31 by vroussea         ###   ########.fr       */
+/*   Updated: 2016/01/06 18:40:46 by vroussea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
 # include "libft.h"
+# define BUF_SIZE 546
 
 typedef struct		s_tetr
 {

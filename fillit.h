f9c9def8f4093a043a46e0e4b83f1f3836bf5911
Valentin ROUSSEAU <vroussea@e2r7p22.42.fr>
2016-01-06 16:50:05 +0100

    normalized

--- fillit.h
+++ fillit.h
@@ -6,23 +6,21 @@
 /*   By: gboudrie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/18 18:05:25 by gboudrie          #+#    #+#             */
-/*   Updated: 2016/01/05 21:45:34 by gboudrie         ###   ########.fr       */
+/*   Updated: 2016/01/06 16:49:32 by vroussea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef FILLIT_H
+
 # define FILLIT_H
 
 # include "libft.h"
 
 typedef struct		s_tetr
 {
-    char			**tetrimino;
+	char			**tetrimino;
 	char			nbr;
-    struct s_tetr	*next;
+	struct s_tetr	*next;
 }					t_tetr;
-
-
 
 #endif

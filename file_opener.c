38ea6de98e67fc3c35b4cb18ba7a3926988208ab
Valentin ROUSSEAU <vroussea@e2r7p22.42.fr>
2016-01-06 23:11:08 +0100

    v1.3

--- file_opener.c
+++ file_opener.c
@@ -6,26 +6,66 @@
 /*   By: vroussea <vroussea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/06 17:07:20 by vroussea          #+#    #+#             */
-/*   Updated: 2016/01/06 19:07:42 by vroussea         ###   ########.fr       */
+/*   Updated: 2016/01/06 23:10:08 by vroussea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
 
-char	*file_opener(char *source)
+static int	file_opener(char *source, char *buf)
 {
-	char	buf[BUF_SIZE];
 	int		fd;
 	int		ret;
 
 	if ((fd = open(source, O_RDONLY)) == -1)
-		return (NULL);
+	{
+		ft_putendl("\n --- Can't open file ! ---\n");
+		return (-1);
+	}
 	ret = read(fd, buf, BUF_SIZE);
-	if (ret  > 545 || ret == -1)
-		return (NULL);
+	if (ret > 545 || ret == -1)
+	{
+		if (ret > 545)
+			ft_putendl("\n --- File too long ! ---\n");
+		else
+			ft_putendl("\n --- Can't read file ! ---\n");
+		return (-1);
+	}
 	buf[ret] = '\0';
 	if (close(fd) == -1)
+	{
+		ft_putendl("\n --- Can't close file ! ---\n");
+		return (-1);
+	}
+	return (ret);
+}
+
+char		*init_buf(char *source)
+{
+	char	*buf;
+	int		size;
+
+	buf = (char *)ft_memalloc(sizeof(BUF_SIZE));
+	if ((size = file_opener(source, buf)) == -1)
 		return (NULL);
+	if (!check_symbols(buf, size))
+	{
+		ft_putendl("\n --- Error symbols ! --- \n");
+		return (NULL);
+	}
+	if (!check_size(buf, size))
+	{
+		ft_putendl("\n --- Error size source ! --- \n");
+		return (NULL);
+	}
+	if (!check_next(tetri) && !check_next2(tetri) && !check_size_tetri(tetri))
+	{
+		ft_putendl("\n --- Error tetrimino ! --- \n");
+		return (NULL);
+	}
 	return (buf);
 }

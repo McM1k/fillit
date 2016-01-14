262f48c8a9a09127033e32e70489a2523e616b00
Gallien BOUDRIE <gboudrie@e2r10p18.42.fr>
2016-01-14 20:20:18 +0100

    backtracker

--- backtracker.c
+++ backtracker.c
@@ -6,7 +6,7 @@
 /*   By: gboudrie <gboudrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/08 18:11:28 by gboudrie          #+#    #+#             */
-/*   Updated: 2016/01/08 19:40:42 by gboudrie         ###   ########.fr       */
+/*   Updated: 2016/01/14 20:19:57 by gboudrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,80 @@ int			how_much_tetr(t_tetr *tetr_ptr)
 	return ((int)(tetr_ptr->nbr - 'A' + 1));
 }
 
-char		**backtrack_tetr(t_tetr *tetr, char **tab)
+static int	check_pos(int x, int y, t_tetr *tetr, int size)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (tetr->tetrimino[i][j] == '#')
+				if ((i + x) >= size || (j + y) >= size
+					|| (i + x) < 0 || (j + y) < 0)
+					return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+static int	check_pos2(t_tetr *start, int x, int y, t_tetr *tetr, int size)
+{
+	int		i;
+	int		j;
+	t_tetr	*current;
+
+	current = start;
+	while (current->nbr < tetr->nbr)
+	{
+		i = 0;
+		while (i < 4)
+		{
+			j = 0;
+			while (j < 4)
+			{
+				if (x >= 0 && y >= 0
+					&& current->tetrimino[i + x][j + y] == '#'
+					&& tetr->tetrimino[i + x][j + y] == '#')
+					return (0);
+				j++;
+			}
+			i++;
+		}
+		current = current->next;
+	}
+	return (1);
+}
+
+int			backtrack_tetr(t_tetr start, t_tetr *tetr, int size)
 {
 	int		i;
 	int		j;
 
 	i = -3;
-	j = -3;
-	while ()
+	while (i < size)
 	{
-
+		j = -3;
+		while (j < size)
+		{
+			if (check_pos(i, j, tetr, size) == 1
+				&& check_pos2(start, i, j, tetr, size) == 1)
+			{
+				tetr->x = i;
+				tetr->y = j;
+				if ((tetr->next) == NULL)
+					return (1);
+				if ((backtrack_tetr(start, tetr->next, size)) == 1)
+					return (1);
+			}
+			j++;
+		}
+		i++;
 	}
+	return (0);
 }

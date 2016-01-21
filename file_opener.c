934066a90147076711b387a058a1943b42d6fb20
Valentin ROUSSEAU <vroussea@e1r2p8.42.fr>
2016-01-21 15:22:10 +0100

    without the error messages

--- file_opener.c
+++ file_opener.c
@@ -6,7 +6,7 @@
 /*   By: vroussea <vroussea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/06 17:07:20 by vroussea          #+#    #+#             */
-/*   Updated: 2016/01/14 21:03:04 by vroussea         ###   ########.fr       */
+/*   Updated: 2016/01/21 15:21:24 by vroussea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,13 @@ static int		file_opener(char *source, char *buf)
 	int		ret;
 
 	if ((fd = open(source, O_RDONLY)) == -1)
-	{
-		ft_putendl("\n --- Can't open file ! ---\n");
 		return (-1);
-	}
 	ret = read(fd, buf, BUF_SIZE);
 	if (ret > 545 || ret == -1)
-	{
-		if (ret > 545)
-			ft_putendl("\n --- File too long ! ---\n");
-		else
-			ft_putendl("\n --- Can't read file ! ---\n");
 		return (-1);
-	}
 	buf[ret] = '\0';
 	if (close(fd) == -1)
-	{
-		ft_putendl("\n --- Can't close file ! ---\n");
 		return (-1);
-	}
 	return (ret);
 }
 
@@ -60,10 +48,7 @@ static int		fill_tetri(t_tetr **start, char *buf)
 	{
 		if (!check_next(current->tetrimino)
 			|| !check_size_tetri(current->tetrimino))
-		{
-			ft_putendl("\n --- Error tetrimino ! --- \n");
 			return (-1);
-		}
 		current = current->next;
 		i--;
 	}
@@ -77,15 +62,9 @@ static int		init_buf(char *source, char *buf)
 	if ((size = file_opener(source, buf)) == -1)
 		return (-1);
 	if (!check_symbols(buf, size))
-	{
-		ft_putendl("\n --- Error symbols ! --- \n");
 		return (-1);
-	}
 	if (!check_size(buf, size))
-	{
-		ft_putendl("\n --- Error size source ! --- \n");
 		return (-1);
-	}
 	return (size);
 }
 
